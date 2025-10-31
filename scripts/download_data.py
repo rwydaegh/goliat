17a1eb60cf1b4a61628feb2f5b57927f5fb65e6a
Robin Wydaeghe <109762571+rwydaegh@users.noreply.github.com>
2025-10-31 16:47:33 +0000

    feat(core): Finalize Linux compatibility changes (#55)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * feat(core): Finalize Linux compatibility changes
    
    This commit includes minor cleanups from pre-commit hooks and finalizes the work for Linux compatibility.
    
    Closes #54

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -17,7 +17,7 @@ def setup_console_logging():
     return logger
 
 
-def download_and_extract_data(base_dir, logger):
+def download_and_extract_data(base_dir, logger, aws=False):
     """
     Downloads folder from Google Drive.
     """
@@ -25,15 +25,22 @@ def download_and_extract_data(base_dir, logger):
     with open(config_path, "r") as f:
         config = json.load(f)
 
-    gdrive_url = config["data_setup"]["gdrive_url"]
     data_dir = os.path.join(base_dir, config["data_setup"]["data_dir"])
-
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
 
-    # Download the folder content
-    logger.info(f"{colorama.Fore.CYAN}Downloading data from {gdrive_url} into {data_dir}...")
-    gdown.download_folder(gdrive_url, output=data_dir, quiet=False)
+    if aws:
+        gdrive_url = config["data_setup"]["gdrive_url_aws"]
+        phantoms_dir = os.path.join(data_dir, "phantoms")
+        if not os.path.exists(phantoms_dir):
+            os.makedirs(phantoms_dir)
+        output_path = os.path.join(phantoms_dir, "duke_posable.sab")
+        logger.info(f"{colorama.Fore.CYAN}Downloading data from {gdrive_url} into {output_path}...")
+        gdown.download(gdrive_url, output=output_path, quiet=False)
+    else:
+        gdrive_url = config["data_setup"]["gdrive_url"]
+        logger.info(f"{colorama.Fore.CYAN}Downloading data from {gdrive_url} into {data_dir}...")
+        gdown.download_folder(gdrive_url, output=data_dir, quiet=False)
 
     logger.info(f"{colorama.Fore.GREEN}Data download complete.")
 
