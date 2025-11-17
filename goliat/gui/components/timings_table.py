9b721e74af67718e987e38cab5e37f63ab868c05
rwydaegh <robin.wydaeghe@ugent.be>
2025-11-17 20:03:40 +0100

    Hide row numbers in timings table

--- goliat/gui/components/timings_table.py
+++ goliat/gui/components/timings_table.py
@@ -35,6 +35,8 @@ class TimingsTable:
             ["Phase", "Subtask", "Mean (s)", "Median (s)", "Min (s)", "Max (s)", "10% (s)", "25% (s)", "75% (s)", "90% (s)"]
         )
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        # Hide vertical header (row numbers)
+        self.table.verticalHeader().setVisible(False)
 
         # Set background color for the table widget to ensure grey background extends
         # beyond the table rows (especially for the first column area)
